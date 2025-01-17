052b7b67cce0dbc63feed54d4babd295fc7e4b06
renato <renatopiresfilho.rvpf@gmail.com>
2025-01-17 13:45:02 -0300

    modified:   src/app/app.component.html 	modified:   src/app/app.component.ts     modified:   src/app/app.routes.ts 	deleted:    src/app/componets/compurna/compurna.component.css 	deleted:    src/app/componets/compurna/compurna.component.html 	deleted:    src/app/componets/compurna/compurna.component.spec.ts 	deleted:    src/app/componets/compurna/compurna.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'va tomar no cu';
+  title = 'sistema da sua escola';
 }
